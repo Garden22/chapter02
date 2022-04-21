465710ebbd529e1da6f9b0b75433a07e6901297b
Garden22 <dalkomjjang@gmail.com>
2022-04-21 14:04:42 +0900

    220421

---
+++ src/com/javaex/ex02/GoodsApp.java
@@ -0,0 +1,22 @@
+package com.javaex.ex02;
+
+public class GoodsApp {
+
+	public static void main(String[] args) {
+		
+		Goods camera = new Goods();
+		Goods laptop = new Goods();
+		Goods cup = new Goods();
+		
+		camera.name = "Nikon";
+		camera.price = 400000;
+		
+		laptop.name = "LG Gram";
+		laptop.price = 900000;
+		
+		cup.name = "mugcup";
+		cup.price = 2000;
+		
+	}
+
+}
