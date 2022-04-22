7778edc98a2ce914cd10e73d59807a05860db5ce
Garden22 <dalkomjjang@gmail.com>
2022-04-22 10:52:47 +0900

    220422 this

--- src/com/javaex/ex04/Point.java
+++ src/com/javaex/ex04/Point.java
@@ -2,33 +2,33 @@ package com.javaex.ex04;
 
 public class Point {
 	
-	private int point_x;
-	private int point_y;
+	private int x;
+	private int y;
 	
 	
 	// setter 메소드
 	public void setX(int x) {
-		point_x = x;
+		this.x = x;
 	}
 	
 	public void setY(int y) {
-		point_y = y;
+		this.y = y;
 	}
 	
 	
 	// getter 메소드
-	public int getX(int x) {
-		return point_x;
+	public int getX() {
+		return x;
 	}
 	
-	public int getY(int y) {
-		return point_y;
+	public int getY() {
+		return y;
 	}
 	
 	
 	//일반 메소드
 	public void draw() {
-		System.out.println("점[x=" + point_x + ", y=" + point_y + "]을 그렸습니다.");
+		System.out.println("점[x=" + x + ", y=" + y + "]을 그렸습니다.");
 	}
 
 }
