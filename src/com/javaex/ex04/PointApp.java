11e792880442db1c0b62122a40e94bc5248f2315
garden <dalkomjjang@gmail.com>
2022-04-22 09:44:12 +0900

    220422

---
+++ src/com/javaex/ex04/PointApp.java
@@ -0,0 +1,19 @@
+package com.javaex.ex04;
+
+public class PointApp {
+
+	public static void main(String[] args) {
+
+		Point ex01 = new Point();
+		ex01.setX(5);
+		ex01.setY(5);
+		ex01.draw();
+		
+		Point ex02 = new Point();
+		ex02.setX(10);
+		ex02.setY(23);
+		ex02.draw();
+				
+	}
+
+}
