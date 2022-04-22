75cdcb1852b28bab856c0e271349b01b08ea481d
Garden22 <dalkomjjang@gmail.com>
2022-04-22 10:32:12 +0900

    220422 주석 추가

--- src/com/javaex/ex04/Point.java
+++ src/com/javaex/ex04/Point.java
@@ -5,6 +5,8 @@ public class Point {
 	private int point_x;
 	private int point_y;
 	
+	
+	// setter 메소드
 	public void setX(int x) {
 		point_x = x;
 	}
@@ -13,6 +15,8 @@ public class Point {
 		point_y = y;
 	}
 	
+	
+	// getter 메소드
 	public int getX(int x) {
 		return point_x;
 	}
@@ -21,6 +25,8 @@ public class Point {
 		return point_y;
 	}
 	
+	
+	//일반 메소드
 	public void draw() {
 		System.out.println("점[x=" + point_x + ", y=" + point_y + "]을 그렸습니다.");
 	}
